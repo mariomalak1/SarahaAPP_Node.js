05ee3e42c729715916914c2bfcfe1006925cbcd2
mario malak <93185168+mariomalak1@users.noreply.github.com>
2025-01-30 15:38:15 +0200

    create models

---
+++ DB/dbModels.js
@@ -0,0 +1,6 @@
+import {UserModel} from "./Models/user.model.js";
+import {MessageModel} from "./Models/message.model.js";
+
+export const dbModels = () => {
+
+}
--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 import Express from "express";
 import dotenv from "dotenv";
 
-import {dbConnection} from "./DB/dbConnection.js"
+import {sequelize, dbConnection} from "./DB/dbConnection.js"
+import {dbModels} from "./DB/dbModels.js"
 // import {router as apiRouter} from "./SRC/Routers/index.router.js";
 // import {ApiError} from "./SRC/Utillis/apiErrors.js";
 // import globalErrorHandle from "./SRC/Middlewares/globalErrorHandle.middleware.js";
@@ -12,8 +13,9 @@ const app = Express();
 
 app.use(Express.json());
 
-const PORT = process.env.PORT;
+// sequelize.sync({"force":true});
 
+const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
     console.log(`server run on port: ${PORT}`);
