d24de03c2e26f92cf9ed09c2338b68df6a9a0c21
mario malak <93185168+mariomalak1@users.noreply.github.com>
2025-02-11 13:26:19 +0200

    verfiy token, before users

--- SRC/Modules/Users/users.router.js
+++ SRC/Modules/Users/users.router.js
@@ -1,12 +1,13 @@
 import {Router} from "express"; 
 
+import {verifyToken} from "../../Utilis/userToken.js";
 import { updateMail, updatePassword, updateProfile, deleteAccount} from "./users.controller.js";
 import { updateMailValidator, updatePasswordValidator , updateProfiledValidator, deleteAccountValidator}
  from "./users.validator.js";
 
 export const router = Router();
 
-router.patch("/updateMail/", updateMailValidator, updateMail);
-router.patch("/updatePassword/", updatePasswordValidator, updatePassword);
-router.patch("/updateProfile/", updateProfiledValidator, updateProfile);
-router.delete("/deleteAccount/", deleteAccountValidator, deleteAccount);
+router.patch("/updateMail/", verifyToken, updateMailValidator, updateMail);
+router.patch("/updatePassword/", verifyToken, updatePasswordValidator, updatePassword);
+router.patch("/updateProfile/", verifyToken, updateProfiledValidator, updateProfile);
+router.delete("/deleteAccount/", verifyToken, deleteAccountValidator, deleteAccount);
